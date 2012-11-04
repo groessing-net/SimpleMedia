8f06f863cf99f770572509f4e1a32cefa992033f
Erik Spaan <erik.spaan@gmail.com>
2012-11-04 19:56:04 +0100

    Using modvar for allowed file extension in upload handler
    In child class method overridden.

--- src/modules/SimpleMedia/lib/SimpleMedia/UploadHandler.php
+++ src/modules/SimpleMedia/lib/SimpleMedia/UploadHandler.php
@@ -17,4 +17,36 @@
 class SimpleMedia_UploadHandler extends SimpleMedia_Base_UploadHandler
 {
     // feel free to add your upload handler enhancements here
+    /**
+     * Determines the allowed file extensions for a given object type.
+     *
+     * @param string $objectType Currently treated entity type.
+     * @param string $fieldName  Name of upload field.
+     * @param string $extension  Input file extension.
+     *
+     * @return array the list of allowed file extensions
+     */
+    protected function isAllowedFileExtension($objectType, $fieldName, $extension)
+    {
+        // determine the allowed extensions
+        $allowedExtensions = array();
+        switch ($objectType) {
+            case 'medium':
+                //$allowedExtensions = array('gif', 'jpeg', 'jpg', 'png', 'pdf', 'doc', 'xls', 'ppt', 'docx', 'xlsx', 'pptx', 'odt', 'ods', 'odp', 'arj', 'zip', 'rar', 'tar', 'tgz', 'gz', 'bz2', 'txt', 'rtf', 'swf', 'flv', 'mp3', 'mp4', 'avi', 'mpg', 'mpeg', 'mov');
+                $allowedExtensions = explode(',', str_replace(' ', '', ModUtil::getVar('SimpleMedia', 'allowedExtensions')));
+                    break;
+        }
+    
+        if (count($allowedExtensions) > 0) {
+            if (!in_array($extension, $allowedExtensions)) {
+                return false;
+            }
+        }
+    
+        if (in_array($extension, $this->forbiddenFileTypes)) {
+            return false;
+        }
+    
+        return true;
+    }
 }
