5322b86a500049e3c8471f2d6dd84533126f879c
gfr <gabriel@freinbichler.net>
2012-12-27 18:14:40 +0100

    throw error if upload directory is not set up correctly

--- src/modules/SimpleMedia/lib/SimpleMedia/Form/Handler/Admin/Medium/Edit.php
+++ src/modules/SimpleMedia/lib/SimpleMedia/Form/Handler/Admin/Medium/Edit.php
@@ -18,4 +18,21 @@
 class SimpleMedia_Form_Handler_Admin_Medium_Edit extends SimpleMedia_Form_Handler_Admin_Medium_Base_Edit
 {
     // feel free to extend the base handler class here
+    public function initialize(Zikula_Form_View $view)
+    {
+        // Check if upload directories exist and if needed create them
+        try {
+            $controllerHelper = new SimpleMedia_Util_Controller(ServiceUtil::getManager());
+            $result = $controllerHelper->checkAndCreateUploadFolder('medium', 'theFile', 'gif, jpeg, jpg, png, pdf, doc, xls, ppt, docx, xlsx, pptx, odt, ods, odp, arj, zip, rar, tar, tgz, gz, bz2, txt, rtf, swf, flv, mp3, mp4, avi, mpg, mpeg, mov');
+        }
+        catch (Exception $e) {
+            return LogUtil::registerError($e->getMessage());
+        }
+        
+        //There is no upload directory or something is wrong
+        if($result != true){
+            throw new Zikula_Exception_Fatal(__("You can not upload media as long the upload directory is not set up correctly!"));
+        }
+       
+    }
 }
